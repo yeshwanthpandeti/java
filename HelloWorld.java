3e1c83bff4358ab709ecc114baf389afc6f36ca0
EC2 Default User <ec2-user@ip-172-31-26-151.ec2.internal>
2022-10-08 09:23:10 +0000

    Pushinhg java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Your First Program
+class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World Man!"); 
+        System.out.println("Hello, World Man!"); 
+        System.out.println("Hello, World Man!"); 
+        System.out.println("Hello, World Man!"); 
+        System.out.println("Hello, World Man!"); 
+    }
+}
